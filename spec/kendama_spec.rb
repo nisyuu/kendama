d252be2122a18ba067cae3b77d36d795e5bc5b79
uichi <37263474+uichi@users.noreply.github.com>
2023-05-31 23:34:54 +0900

    スケルトンに入っていたテストの一つをスキップさせるようにする

--- spec/kendama_spec.rb
+++ spec/kendama_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Kendama do
     expect(Kendama::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  xit "does something useful" do
     expect(false).to eq(true)
   end
 end
