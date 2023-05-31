d1b7d829efff5fdbb9b9674af8442e34cfb9676c
uichi <37263474+uichi@users.noreply.github.com>
2023-05-31 22:32:24 +0900

    bundle gem実行によるgemのスケルトン生成

---
+++ lib/kendama.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kendama/version"
+
+module Kendama
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kendama_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kendama do
+  it "has a version number" do
+    expect(Kendama::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
